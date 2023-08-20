e3249a6621c6d87ec33ad9ea902f0e11d62f8a08
martha espinel <marthaingesiso@gmail.com>
2023-08-20 17:37:07 -0500

    second commit

--- angular-tour-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-heroes/src/app/heroes/heroes.component.ts
@@ -8,3 +8,33 @@ import { Component } from '@angular/core';
 export class HeroesComponent {
 
 }
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'heroes.component.ts';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Vision' },
+      { id: 2, name: 'Thor' },
+      { id: 3, name: 'Hulk' },
+      { id: 4, name: 'Dr. Strange' },
+      { id: 5, name: 'Spiderman' },
+      { id: 6, name: 'Iron Man' },
+      { id: 7, name: 'Capitan America' },
+      { id: 8, name: 'Ant Man' },
+      { id: 9, name: 'Black Widow' },
+      { id: 10, name: 'Scarlet Witch' },
+      { id: 11, name: 'Black Panther' },
+      { id: 12, name: 'Deadpool' }
+    ];
+    return {heroes};
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
