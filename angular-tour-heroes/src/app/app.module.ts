a75e73ed363bf6cecf66a444143d476236842b8d
martha espinel <marthaingesiso@gmail.com>
2023-08-27 21:16:15 -0500

    second commit

--- angular-tour-heroes/src/app/app.module.ts
+++ angular-tour-heroes/src/app/app.module.ts
@@ -6,22 +6,40 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import {MatCardModule} from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
 
-
-
-@NgModule (
-  imports: 
-    BrowserModule
-    FormsModule
-    bootstrap: [ AppComponent ]
-    import  "@angular/material";
-),
-  declarations: (
-    AppComponent
-    DashboardComponent
-    HeroesComponent
-    HeroDetailComponent
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
     MessagesComponent
-  )
-
- export class AppModule {}
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
